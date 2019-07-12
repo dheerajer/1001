0cf300e31aae4a698de5f4c36e333bf763bfb61e
Dheeraj Yadav <dheeraj.er2@gmail.com>
2019-07-12 17:00:39 +0530

    Some changes

--- src/test/java/com/route4me/TestClasses.java
+++ src/test/java/com/route4me/TestClasses.java
@@ -7,34 +7,34 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestClasses {
-	
-	@Test(priority=1, groups={"admin"})
-	public  void testMethod1() {
+
+	@Test(priority = 1, groups = { "admin" })
+	public void testMethod1() {
 		System.out.println("####################testMethod1");
-		int x=0;
-		int y=0;
-		System.out.println(x/y);
+		int x = 0;
+		int y = 0;
+		System.out.println(x / y);
 	}
-	
-	@Test(priority=2)
-	public  void testMethod2() {
+
+	@Test(priority = 2)
+	public void testMethod2() {
 		System.out.println("####################testMethod2");
 	}
-	
-	@Test(dependsOnMethods={"testMethod1"})
-	public  void testMethod3() {
+
+	@Test(dependsOnMethods = { "testMethod1" })
+	public void testMethod3() {
 		System.out.println("####################testMethod3");
 		System.out.println();
 	}
-	
-	@Test(groups={"admin"})
+
+	@Test(groups = { "admin" })
 	@Parameters({ "sUsername", "sPassword" })
-	public  void testMethod4( String uName, String uPwd) {
+	public void testMethod4(String uName, String uPwd) {
 		System.out.println("####################testMethod4");
-		System.out.println("Username:"+uName +" Password"+uPwd);
-	
+		System.out.println("Username:" + uName + " Password" + uPwd);
+
 	}
-	
+
 	@Test(groups = { "admin" })
 	public void testMethod5() {
 		System.out.println("####################testMethod5");
@@ -42,43 +42,49 @@ public class TestClasses {
 		int x = str.length;
 		// str.length();
 	}
-	
-	@Test(groups={"user","admin"})
-	public  void testMethod6() {
+
+	@Test(groups = { "user", "admin" })
+	public void testMethod6() {
 		System.out.println("####################testMethod6");
 	}
-	
-	
+
 	@DataProvider(name = "Authentication")
 	public static Object[][] credentials() {
-		return new Object[][] { { "testuser_1", "Test@123" }, { "testuser_2", "Test@123" }};
+		return new Object[][] { { "testuser_1", "Test@123" }, { "testuser_2", "Test@123" } };
 
 	}
-	 
-	@Test(groups={"user","admin"}, dataProvider = "Authentication")
+
+	@Test(groups = { "user", "admin" }, dataProvider = "Authentication")
 	public void testMethod7(String uName, String uPwd) {
 		System.out.println("####################testMethod7");
-		System.out.println("Username:"+uName +" Password"+uPwd);
-		
+		System.out.println("Username:" + uName + " Password" + uPwd);
+
 	}
-	
-	@Test(dataProvider = "Authentication", dataProviderClass=Data.class)
+
+	@Test(dataProvider = "Authentication", dataProviderClass = Data.class)
 	public void testMethod8(String uName, String uPwd) {
 		System.out.println("####################testMethod8");
-		System.out.println("Username:"+uName +" Password"+uPwd);
-		
+		System.out.println("Username:" + uName + " Password" + uPwd);
+
 	}
-	
-	@Test(enabled=false)
+
+	@Test(enabled = false)
 	public void testMethod9() {
 		System.out.println("####################testMethod9");
 	}
 
 	@Test
-	public  void testMethod() {
+	public void testMethod() {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-	WebDriver driver=new ChromeDriver();
-   	driver.get("https://www.softwaretestingmaterial.com/testng-introduction/");
-		
-	
-}}
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.softwaretestingmaterial.com/testng-introduction/");
+
+	}
+
+	@Test
+	public void gotoUrl() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.softwaretestingmaterial.com/testng-introduction/");
+	}
+}
